a94d7fabd51b094796e7aa8a4994afa9f4400da5
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-28 05:23:39 +0000

    Automatically backed up by Learn

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      t.integer :artist_id
+      t.belongs_to :artist
 
       t.timestamps null: false
     end
